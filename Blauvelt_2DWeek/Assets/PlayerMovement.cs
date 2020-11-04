54672d754093d215947a1b17fb12b01bf51f17cf
Aubrie Blauvelt <aublauve@iu.edu>
2020-11-03 21:50:11 -0500

    Player Movement
    Created PlayerMovement script

---
+++ Blauvelt_2DWeek/Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody2D rB2D;
+
+    public float runSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rB2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        rB2D.velocity = new Vector2(horizontalInput * runSpeed * Time.deltaTime, rB2D.velocity.y); 
+    }
+}
